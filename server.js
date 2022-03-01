4fb907bea89d2df2d2cc07ee89b6ff3a13a51523
AlessandroB96 <abelaj7196@gmail.com>
2022-03-01 16:09:51 -0500

    added CRUD operation routes

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const inputCheck = require('./utils/inputCheck');
+
 //connecting to mysql database 
 const mysql = require('mysql2');
 
@@ -35,38 +37,106 @@ const db = mysql.createConnection(
 // });
 
 
-//CREATE QUERY FOR READ OPERATION 
-//GET a single candidate 
-db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
-    if (err) {
-    console.log(err);
+//GET all candidates
+
+app.get('/api/candidates', (req, res) => {
+  const sql = `SELECT * FROM candidates`;
+
+    db.query(sql, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
     }
-    console.log(row);
+    res.json({
+        message: 'success',
+        data: rows
+    });
+});
 });
 
-//CREATE QUERY FOR DELETE OPERATION 
-//Delete a candidate 
-// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {  //question mark represents a 'prepared statement'. The question mark is a placeholder so when you make a sql statement it can take different values. The '1' is just a hardcoded value for now. If you want to add additional paramenters, make an array
+
+//CREATE QUERY FOR READ OPERATION 
+//GET a single candidate 
+app.get('/api/candidate/:id', (req, res) => {
+
+    const sql = `SELECT * FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+
+    db.query(sql,params, (err, row) => {
+        if (err) {
+        res.status(400).json({error: err.message});
+        return;
+        }
+        res.json({
+            message: 'success',
+            data: row
+        });
+    });
+});
+
+// CREATE QUERY FOR DELETE OPERATION 
+// Delete a candidate 
+app.delete('/api/candidate/:id', (req,res) => {
+
+    const sql = `DELETE FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+
+    db.query(sql, params, (err, result) => {  //question mark represents a 'prepared statement'. The question mark is a placeholder so when you make a sql statement it can take different values. The '1' is just a hardcoded value for now. If you want to add additional paramenters, make an array
+        if (err) {
+        res.statusMessage(400).json({ error: res.message});
+        } else if (!result.affectedRows) {
+        res.json({
+            message: 'Candidate Not Found'
+        });
+        } else {
+            res.json({
+                message: 'deleted',
+                changes: result.affectedRows,
+                id: req.params.id
+            });
+        }
+    });
+}); 
+
+//create a candidate 
+
+app.post('/api/candidate', ({body}, res) => {
+
+    const errors = inputCheck(body, 'first_name','last_name','industry_connected');
+    if (errors) {
+        res.status(400).json({ error: errors });
+        return;
+    }
+    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
+                    VALUES (?,?,?)`;
+    const params = [body.first_name, body.last_name, body.industry_connected];
+
+    db.query(sql, params, (err, result) => {
+    if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+    }
+    res.json({
+        message: 'success',
+        data: body
+    });
+});
+});
+
+// CREATE QUERY FOR CREATE OPERATION 
+// Create a candidate 
+
+// const sql = `INSERT INTO candidates (id,first_name,last_name,industry_connected)
+//                 VALUES (?,?,?,?)`;
+
+// const params = [1,'Ronald','Firbank',1];
+
+// db.query(sql, params, (err, result) => {
 //     if (err) {
-//     console.log(err);
+//         console.log(err);
 //     }
 //     console.log(result);
 // });
-
-//CREATE QUERY FOR CREATE OPERATION 
-//Create a candidate 
-
-const sql = `INSERT INTO candidates (id,first_name,last_name,industry_connected)
-                VALUES (?,?,?,?)`;
-
-const params = [1,'Ronald','Firbank',1];
-
-db.query(sql, params, (err, result) => {
-    if (err) {
-        console.log(err);
-    }
-    console.log(result);
-});
 
 // Default response for any other request (Not Found) (RANDOM API ENDPOINT THAT DOESN'T EXIST)
 app.use((req, res) => {
