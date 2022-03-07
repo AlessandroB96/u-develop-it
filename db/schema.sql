f1789cd38e1a18d16105d679c83644d683ad89cf
AlessandroB96 <abelaj7196@gmail.com>
2022-03-07 18:20:50 -0500

    added relational tables

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,7 @@
+-- delete the tables every time you run the schema.sql file to start with a clean slate
+DROP TABLE IF EXISTS candidates;
+DROP TABLE IF EXISTS parties;
+
 CREATE TABLE parties (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
@@ -8,6 +12,10 @@ CREATE TABLE candidates (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    industry_connected BOOLEAN NOT NULL
+    party_id INTEGER,
+    industry_connected BOOLEAN NOT NULL,
+    CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE SET NULL 
+    --constraint lien allows us to flag party_id as a foriegn key for linking tables tells sql which table and feild it references
+    --on delete set null tells sql to set candidates party_id to null if row in parties is deleted
 );
 
